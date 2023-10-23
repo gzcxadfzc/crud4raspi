01a91167725ad7ba58afbc8f99d2a3780c39efec
Sy P <yongsa0221@gmail.com>
2023-10-24 03:23:45 +0900

    feat : 테이블 조회 매핑 추가

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -14,9 +14,28 @@ public class HomeController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @GetMapping("/home")
-    public List<Map<String, Object>> home() {
-        StringBuilder outputText = new StringBuilder();
+    @GetMapping("/")
+    public String home() {
+        return "20192681 박순용 /instructor /teaches /student /takes 테이블 조회 가능 ";
+    }
+
+    @GetMapping("/instructor")
+    public List<Map<String, Object>> instructor() {
         return jdbcTemplate.queryForList("select * from instructor");
+    }
+
+    @GetMapping("/student")
+    public List<Map<String, Object>> student() {
+        return jdbcTemplate.queryForList("select * from student");
+    }
+
+    @GetMapping("/takes")
+    public List<Map<String, Object>> takes() {
+        return jdbcTemplate.queryForList("select * from takes");
+    }
+
+    @GetMapping("/teaches")
+    public List<Map<String, Object>> teaches() {
+        return jdbcTemplate.queryForList("select * from teaches");
     }
 }
